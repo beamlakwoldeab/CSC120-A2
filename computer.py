7c348d4b0ac6a3157d75ed4d00579d26e234e62a
beamlakwoldeab <bwoldeab@smith.edu>
2023-02-08 21:09:27 -0500

    Updated version

--- computer.py
+++ computer.py
@@ -1,10 +1,48 @@
 class Computer:
+    description:str
+    processor_type:str
+    hard_drive_capacity:int
+    memory:str
+    operating_system: str
+    year_made: int
+    price : int
+    def __init__(self, description, processor_type, hard_drive_capacity, memory,operating_system,year_made, price):
+        self.description=description
+        self.processor_type=processor_type
+        self.hard_drive_capacity=hard_drive_capacity
+        self.memory=memory
+        self.operating_system=operating_system
+        self.year_made=year_made
+        self.price=price
 
-    # What attributes will it need?
+    def updatePrice(self, newPrice):
+        self.price=newPrice
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+        # updating the price of an item in the inventory
+    def updateOs (self, newOs):
+        self.operating_system=newOs
+    
+    def printDetails(self):
+        print(self.description,self.processor_type, self.hard_drive_capacity,self.memory,self.operating_system,self.year_made, self.price)
+        # print(self.processor_type)
+        # print(self.hard_drive_capacity)
+        # print(self.memory)
+        # print(self.operating_system)
+        # print(self.year_made)
+        # print(self.price)
 
-    # What methods will you need?
+    def refurbish(self) -> None:
+        # refurbishing a computer (update price based on age of machine, optionally update OS)
+        if 0 < self.year_made < 2000:
+            self.price=0
+        elif 2000 <= self.year_made < 2012:
+            self.price=250
+        elif 2012<= self.year_made < 2018:
+            self.price=550
+        else:
+            self.price=1000
+
+        #this is not working
+   
+c1=Computer('Macbook pro!!!!!!!', 'Intel', '5gb', '5gb','windows', 2003,400)
+c1.printDetails()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,32 @@
-class ResaleShop:
+from computer import *
 
-    # What attributes will it need?
+class ResaleShop():
+    inventory = list
+    def __init__ (self):
+        self.inventory=[]
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def buy(self, computer: Computer):
+        self.inventory.append(computer)
 
-    # What methods will you need?
+        # buying a computer (add to inventory)
+        
+    def sell(self, computer: Computer):
+        # selling a computer (remove from inventory)
+        self.inventory=self.inventory.remove(computer)
+
+    def printInventory(self, computer:Computer):     
+        #  printing the inventory
+        for computer in self.inventory:
+            computer.printDetails() # make a print details function in computer class
+        # print(self.inventory)
+
+def main():
+    c1=Computer('Macbook pro!!!!!!!', 'Intel', '5gb', '5gb','windows', 2003, 400)
+    store=ResaleShop()
+    store.buy(c1)
+    store.printInventory(c1)
+    # print(store)
+    # print(c1.description)
+    # c1.refurbish()
+    # print(c1.price)
+main()
